4e334c6fec921837b92b41e2475c66c3d5e6bc25
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-05-12 16:31:02 +1200

    display meaningful error messages for rejected input

--- tracker-cli.js
+++ tracker-cli.js
@@ -13,12 +13,18 @@ function ask(q) {
 }
 
 async function addNewSession() {
-    // ADD THROW AND CATCH FOR ERRORS
-    const topic = await ask('Enter Session Topic: ');
-    validateTopic(topic);
+    let minutes;
 
-    const duration = await ask('Enter Session Duration: ');
-    validateDuration(duration);
+    try {
+        const topic = await ask('Enter Session Topic: ');
+        validateTopic(topic);
+
+        const duration = await ask('Enter Session Duration: ');
+        minutes = validateDuration(duration);
+
+    } catch(err) {
+        console.log(err + ', Try Again');
+    }
 
     rl.close();
 }
@@ -27,33 +33,30 @@ function validateTopic(topic) {
     // VALIDATE
     // TOPIC: MUST NOT BE EMPTY OR WHITESPACE
 
-    if (topic.trim() === '') { return console.log("error")}
+    if (topic.trim() === '') { 
+        throw new Error('Topic must not be empty');
+    }
 }
 
 function validateDuration(duration) {
     // VALIDATE
     // DURATION: MUST NOT BE NON-POSITIVE NUMBER, MUST NOT USE INCORRECT MEASUREMENT
-    // EXAMPLE STRING: 4h:20m
-    
+
     const validMeasurements = ['h', 'm'];
 
     const segments = duration.split(':');
     for (let segment of segments) {
         // Check if last element in string is valid measurement e.g. 'h' or 'm' 
         if (!validMeasurements.includes(segment[segment.length - 1].toLowerCase())) {
-            console.log('Measurement Error');
-            return;
+            throw new Error('Ensure each segment ends with correct time measurement (h or m): e.g. 4h:20m, 20m')
         }
         const number = Number(segment.slice(0, segment.length - 1));
         if (Number.isNaN(number)) {
-            console.log('Formatting Error');
-            return;
+            throw new Error('Ensure correct formatting is used e.g. 4h:20m, 20m');
         }
-        console.log(number);
         if (number <= 0) {
-            console.log('non-positive value error');
-            return;
-        }
+            throw new Error('Number must be a positive value');
+        }  
     }
 }
 
