7d691d05599cd8c25288065c117774c2aa379ffd
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-05-12 15:47:18 +1200

    add input gathering from user for topic and duration

--- tracker-cli.js
+++ tracker-cli.js
@@ -1,4 +1,23 @@
-let topic;
-let duration;
 let totalDuration;
 const sessions = [];
+
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function ask(q) {
+    return new Promise(resolve => rl.question(q, resolve));
+}
+
+async function addNewSession() {
+    const topic = await ask('Enter Session Topic: ');
+    const duration = await ask('Enter Session Duration: ');
+
+    
+    rl.close();
+}
+
+addNewSession();
