77cdeefdd85f64cc871bbe4d44e232cf645e472c
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-05-12 16:15:48 +1200

    validate input to reject invalid topic and duration values

--- tracker-cli.js
+++ tracker-cli.js
@@ -13,11 +13,48 @@ function ask(q) {
 }
 
 async function addNewSession() {
+    // ADD THROW AND CATCH FOR ERRORS
     const topic = await ask('Enter Session Topic: ');
-    const duration = await ask('Enter Session Duration: ');
+    validateTopic(topic);
 
-    
+    const duration = await ask('Enter Session Duration: ');
+    validateDuration(duration);
+
     rl.close();
+}
+
+function validateTopic(topic) {
+    // VALIDATE
+    // TOPIC: MUST NOT BE EMPTY OR WHITESPACE
+
+    if (topic.trim() === '') { return console.log("error")}
+}
+
+function validateDuration(duration) {
+    // VALIDATE
+    // DURATION: MUST NOT BE NON-POSITIVE NUMBER, MUST NOT USE INCORRECT MEASUREMENT
+    // EXAMPLE STRING: 4h:20m
+    
+    const validMeasurements = ['h', 'm'];
+
+    const segments = duration.split(':');
+    for (let segment of segments) {
+        // Check if last element in string is valid measurement e.g. 'h' or 'm' 
+        if (!validMeasurements.includes(segment[segment.length - 1].toLowerCase())) {
+            console.log('Measurement Error');
+            return;
+        }
+        const number = Number(segment.slice(0, segment.length - 1));
+        if (Number.isNaN(number)) {
+            console.log('Formatting Error');
+            return;
+        }
+        console.log(number);
+        if (number <= 0) {
+            console.log('non-positive value error');
+            return;
+        }
+    }
 }
 
 addNewSession();
